1d4fdd5d3f7b09cef0ba0c0bde85608939477fe3
Pachrijam <paul830a@gmail.com>
2026-06-08 16:45:42 -0400

    initial commit

---
+++ src/dataReader.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class dataReader {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        File file = new File("CrimeData.csv");
+        
+        System.out.println("License Plate: ");
+        String licensePlate = scan.nextLine();
+        
+        
+    }
+}
